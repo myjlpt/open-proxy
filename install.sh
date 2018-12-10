7ea462522e76dc4726a45fbd115592539a69775d
gfw-breaker <lixiaoming0513@gmail.com>
2018-12-10 19:39:54 +0800

    -

--- install.sh
+++ install.sh
@@ -11,12 +11,17 @@ mkdir -p /usr/share/nginx/html
 
 server_ip=$(ifconfig | grep "inet addr" | sed -n 1p | cut -d':' -f2 | cut -d' ' -f1)
 
-for f in $(ls sites); do
-	sed -i "s/local_server_ip/$server_ip/g" sites/$f
+for f in $(ls sites/*.conf); do
+	sed -i "s/local_server_ip/$server_ip/g" $f
+done
+
+for f in $(ls pages/*.html); do
+	sed -i "s/local_server_ip/$server_ip/g" $f
 done
 
 cp common/* /etc/nginx/
 cp sites/* /etc/nginx/conf.d
+cp pages/* /usr/share/nginx/html
 
 mv /etc/init.d/kaltura-nginx /etc/init.d/nginx
 
